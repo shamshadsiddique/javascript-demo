189759ff12d08bd716a8fd824c87f999ccd3e91c
Shamshad Siddique <shamshadpendar@gmail.com>
2024-01-11 10:18:37 +0000

    detatype

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
 console.log("shamshad")
+accountId = "12345"
+accountEmail = "shamshad@gmail.com"
+accountPassword = "123456790"
+accountCity = "Lucknow"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ demoTwo.js
@@ -0,0 +1,10 @@
+let name = "shamshad" /* string*/
+let age = 40  /* number*/
+let isLoggedin = false /* Bo0lian */
+/* Null > object standalone value */
+
+/*undefined => undefined */
+/* symbol unique*/
+/* Object */
+
+console.log(typeof age)
