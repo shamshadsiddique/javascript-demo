1c66ed786147be76401b369196e80e77bbb38a5f
Shamshad Siddique <shamshadpendar@gmail.com>
2024-01-11 10:51:21 +0000

    dataType

---
+++ demoThree.js
@@ -0,0 +1,23 @@
+let score = true
+console.log(typeof (score))
+
+let valueInNumber = Number (score) 
+console.log(typeof score)
+console.log(valueInNumber)
+
+/* "33" => 33 */
+/* "33asd" null */
+/* true =>1 false =>0 */
+
+let isLoggedin = "asd"
+
+let booleanIsLogedin = Boolean(isLoggedin)
+console.log(booleanIsLogedin)
+
+/* 1 => true, 0 => false, "" =>true */
+
+let someNumber = 33
+let stringNUmber = String(someNumber)
+console.log(stringNUmber)
+console.log(typeof stringNUmber)
+
